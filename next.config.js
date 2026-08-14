d4b3bab7a5cab10b9060e16de66490b45b703031
Sushmit Kumar Satapathy <sushmitkumarsatapathy@gmail.com>
2026-08-15 00:47:15 +0530

    Final Version 1.3.0

--- next.config.js
+++ next.config.js
@@ -26,6 +26,9 @@ const nextConfig = {
     ],
     minimumCacheTTL: 31536000, // 1 year cache for remote images
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
